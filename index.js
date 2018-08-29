8b19717d56d2ae208da35d9592f3a79e4a47a22c
John Wohlbier <john.wohlbier@engilitycorp.com>
2018-08-28 22:32:35 -0400

    change return type to html

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ var http = require('http');
 
 var server = http.createServer(function(request, response) {
 
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hola World!");
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.end("<p><strong>Hola World!</strong></p>");
 
 });
 
