9b8d18d566b7a7211c99167e5e1ff07ae9f6c309
John Wohlbier <john.wohlbier@engilitycorp.com>
2018-08-28 22:39:12 -0400

    more html

--- index.js
+++ index.js
@@ -3,7 +3,17 @@ var http = require('http');
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/html"});
-    response.end("<p><strong>Hola World!</strong></p>");
+    response.write('<!DOCTYPE html>'+
+		   '<html>'+
+		   ' <head>'+
+		   ' <meta charset="utf-8" />'+
+		   ' <title>My Node.js page!</title>'+
+		   ' </head>'+ 
+		   ' <body>'+
+		   ' <p>Here is a paragraph of <strong>HTML</strong>!</p>'+
+		   ' </body>'+
+		   '</html>');
+    response.end();
 
 });
 
